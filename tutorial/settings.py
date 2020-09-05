aa6f6328025d36f05c1af27ccb395edb455ba673
Codi Huston (Home) <codi.huston540@gmail.com>
2020-09-04 21:39:49 -0500

    feat: add pagination (DRF feature)

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -118,3 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Rest Framework Config (Manually added)
+REST_FRAMEWORK = {
+  'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+  'PAGE_SIZE': 10
+}
