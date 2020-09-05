5b9f8c5dca3dbb5b933babb3b0597890094937ae
Codi Huston (Home) <codi.huston540@gmail.com>
2020-09-04 21:37:18 -0500

    feat: implement URL conf for the users and groups

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -13,9 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from tutorial.quickstart import views
 
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing (since we are using ViewSets,
+# we can register them with the router to auto-generate URL configuration for
+# the API). If we needed more control over API URLs, we can simply drop
+# down to using regular class-based views, and writing the URL conf explicitly.
+# Additionally, we include login URLs for the browsable API
 urlpatterns = [
-    path('admin/', admin.site.urls),
+  path('', include(router.urls)),
+  path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
