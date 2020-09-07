d7a772f3eeeb5e0fb00c5a14e4415bbab5d78224
Codi Huston (Home) <codi.huston540@gmail.com>
2020-09-07 16:09:17 -0500

    feat: require auth to create snippets
    - API to view snippets and users is read-only if not logged in
    - if logged in, creating a snippet associates it with the logged-in user
    - the user API list displays their snippets, but only the ID of the
    snippet

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from snippets.models import Snippet, LANGUAGE_CHOICES, SYTLE_CHOICES
 
+class UserSerializer(serializers.ModelSerializer):
+  snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+  class Meta:
+    model = User
+    fields = ['id', 'username', 'snippets']
+
 class SnippetSerializer(serializers.ModelSerializer):
+  # now that snippets are associated with the user that created them 
+  # (see views/SnippetList), we need to reflect that here by adding this field
+  # The untyped `ReadOnlyField` will only be used for serializing, but never
+  # when mutating a model instance
+  owner = serializers.ReadOnlyField(source='owner.username')
+
   class Meta:
     model = Snippet
-    fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+    # also add owner here
+    fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
--- snippets/views.py
+++ snippets/views.py
@@ -1,18 +1,38 @@
-from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework import generics, permissions
 from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 from rest_framework_xml.renderers import XMLRenderer
 
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import SnippetSerializer, UserSerializer
 
-# Create your views here.
+class UserList(generics.ListAPIView):
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
 
 class SnippetList(generics.ListCreateAPIView):
+  permission_classes = [permissions.IsAuthenticatedOrReadOnly]
   queryset = Snippet.objects.all()
   serializer_class = SnippetSerializer
   renderer_classes = [BrowsableAPIRenderer, JSONRenderer, XMLRenderer]
 
+  def perform_create(self, serializer):
+    """
+    We need to override this, because as incoming data comes in, we need
+    to associate it with a user. With the line below, the '.create()' method
+    in the serializer will now be passed an additional 'owner' field, along
+    with the validated data from the request.
+
+    After this change, now you need to update the serializer to handle this.
+    """
+    serializer.save(owner=self.request.user)
+
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+  permission_classes = [permissions.IsAuthenticatedOrReadOnly]
   queryset = Snippet.objects.all()
   serializer_class = SnippetSerializer
   renderer_classes = [BrowsableAPIRenderer, JSONRenderer, XMLRenderer]
