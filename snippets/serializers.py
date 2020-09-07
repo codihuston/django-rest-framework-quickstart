448a1b14fbb256f5009a26961817d640b843593c
Codi Huston (Home) <codi.huston540@gmail.com>
2020-09-07 17:14:45 -0500

    feat: implement href relationships
    - add api root
    - users / snippets now include hrefs to some of their relationships

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -2,21 +2,24 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from snippets.models import Snippet, LANGUAGE_CHOICES, SYTLE_CHOICES
 
-class UserSerializer(serializers.ModelSerializer):
-  snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+  snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
   class Meta:
     model = User
     fields = ['id', 'username', 'snippets']
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
   # now that snippets are associated with the user that created them 
   # (see views/SnippetList), we need to reflect that here by adding this field
   # The untyped `ReadOnlyField` will only be used for serializing, but never
   # when mutating a model instance
   owner = serializers.ReadOnlyField(source='owner.username')
+  # replace the line above with the line below to get href to user instead
+  #owner = serializers.HyperlinkedIdentityField(view_name='user-detail')
+  highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
 
   class Meta:
     model = Snippet
     # also add owner here
-    fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner']
+    fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner', 'highlight']
--- snippets/views.py
+++ snippets/views.py
@@ -1,11 +1,21 @@
 from django.contrib.auth.models import User
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, renderers
 from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 from rest_framework_xml.renderers import XMLRenderer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 from snippets.models import Snippet
 from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer, UserSerializer
+
+@api_view(['GET'])
+def api_root(request, format=None):
+  return Response({
+    'users': reverse('user-list', request=request, format=format),
+    'snippets': reverse('snippet-list', request=request, format=format)
+  })
 
 class UserList(generics.ListAPIView):
   queryset = User.objects.all()
@@ -37,3 +47,11 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
   queryset = Snippet.objects.all()
   serializer_class = SnippetSerializer
   renderer_classes = [BrowsableAPIRenderer, JSONRenderer, XMLRenderer]
+
+class SnippetHighlight(generics.GenericAPIView):
+  queryset = Snippet.objects.all()
+  renderer_classes = [renderers.StaticHTMLRenderer]
+
+  def get(self, request, *args, **kwargs):
+    snippet = self.get_object()
+    return Response(snippet.highlighted)
