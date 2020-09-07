b1a1b8a6d9559b3a5196da62b56ee9af6a87db06
Codi Huston (Home) <codi.huston540@gmail.com>
2020-09-07 15:22:37 -0500

    refactor: abstract mixins out, replacing with generic views
    - wow this is pretty neat

--- snippets/views.py
+++ snippets/views.py
@@ -1,5 +1,3 @@
-
-from rest_framework import mixins
 from rest_framework import generics
 from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 from rest_framework_xml.renderers import XMLRenderer
@@ -9,27 +7,12 @@ from snippets.serializers import SnippetSerializer
 
 # Create your views here.
 
-class SnippetList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
   queryset = Snippet.objects.all()
   serializer_class = SnippetSerializer
   renderer_classes = [BrowsableAPIRenderer, JSONRenderer, XMLRenderer]
 
-  """
-  List all code snippets, or create a new snippet
-  """
-  def get(self, request, *args, **kwargs):
-    return self.list(request, *args, **kwargs)
-
-  def post(self, request, *args, **kwargs):
-    return self.create(request, *args, **kwargs)
-
-class SnippetDetail(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
-
-  def get(self, request, *args, **kwargs):
-    return self.retrieve(request, *args, **kwargs)
-
-  def put(self, request, *args, **kwargs):
-    return self.update(request, *args, **kwargs)
-
-  def delete(self, request, *args, **kwargs):
-    return self.destroy(request, *args, **kwargs)
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Snippet.objects.all()
+  serializer_class = SnippetSerializer
+  renderer_classes = [BrowsableAPIRenderer, JSONRenderer, XMLRenderer]
