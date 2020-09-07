17143be04039c91137a239924d8cb3787f707dc2
Codi Huston (Home) <codi.huston540@gmail.com>
2020-09-07 15:09:06 -0500

    refactor: delete unused ref

--- snippets/views.py
+++ snippets/views.py
@@ -2,7 +2,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.http import Http404
-from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 from rest_framework_xml.renderers import XMLRenderer
 
