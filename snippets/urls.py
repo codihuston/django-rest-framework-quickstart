aed35306889a5835c335df922bcb247bdcb139f6
Codi Huston (Home) <codi.huston540@gmail.com>
2020-09-08 13:06:16 -0500

    refactor: trailing slash optional
    - some browsers append a trailing slash by default
    - after making trailing slashes optional in the previous commit, the app could not process endpoints ending with a trailing slash
    - updating this regex fixes that

--- snippets/urls.py
+++ snippets/urls.py
@@ -4,8 +4,8 @@ from rest_framework.routers import DefaultRouter
 from snippets import views
 
 router = DefaultRouter(trailing_slash=False)
-router.register(r'snippets', views.SnippetViewSet)
-router.register(r'users', views.UserViewSet)
+router.register(r'snippets/?', views.SnippetViewSet)
+router.register(r'users/?', views.UserViewSet)
 
 urlpatterns = [
   path('', include(router.urls))
