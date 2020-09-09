84b25939f501cb9b128e059b84a6e21e61c0f1f7
Codi Huston (Home) <codi.huston540@gmail.com>
2020-09-08 19:18:09 -0500

    refactor: set snippet owner to href instead
    - was previously set to username

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -7,16 +7,17 @@ class SnippetSerializer(serializers.HyperlinkedModelSerializer):
   # now that snippets are associated with the user that created them 
   # (see views/SnippetList), we need to reflect that here by adding this field
   # The untyped `ReadOnlyField` will only be used for serializing, but never
-  # when mutating a model instance
-  user_id = serializers.ReadOnlyField(source='owner.id')
+  # when mutating a model instance. We specify which value to display here.
+  # Note that "owner" is set to a user instance as defined on the Snippet model
+  #owner = serializers.ReadOnlyField(source='owner.username')
   # replace the line above with the line below to get href to user instead
-  #owner = serializers.HyperlinkedIdentityField(view_name='user-detail')
+  owner = serializers.HyperlinkedRelatedField(view_name='user-detail', read_only=True)
   highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
 
   class Meta:
     model = Snippet
     # also add owner here
-    fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'user_id', 'highlight']
+    fields = ['id', 'title', 'code', 'linenos', 'language', 'style', 'owner', 'highlight']
 
 
 class PermissionSerializer(serializers.HyperlinkedModelSerializer):
